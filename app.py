bde952effbb5aa0d8fc2e49780abdcba6d2a51db
Syed Ali <syedali3267@gmail.com>
2021-08-16 12:28:33 +0100

    output is now dispalyed on report page

--- app.py
+++ app.py
@@ -6,78 +6,104 @@ import dash_html_components as html
 
 import pandas as pd
 import plotly.express as px
-
+print(dcc.__version__)
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.config.suppress_callback_exceptions = True
+
+index_page = html.Div(
+    # I added this id attribute
+    id='index_page',
+    children=[
+                dcc.Link('Go to Main Page', href='/page-1'),
+                html.Br(),
+                dcc.Link('Go to Report Page', href='/page-2'),
+            ],
+    # I added this style attribute
+    style={'display': 'block', 'line-height':'0', 'height': '0', 'overflow': 'hidden'}
+)
+
+page_1_layout = html.Div(
+    # I added this id attribute
+    id='page_1_layout',
+    children=[
+        html.H1('Main Page'),
+        html.H4("Insert a value between 0 - 1000 below"),
+        html.Div([
+            "Input: ",
+            dcc.Input(id='my-input', value='', type='number', min=0, max=1000, persistence=True, persistence_type='memory')
+        ]),
+        html.Br(),
+        html.Div(id='my-output'),
+
+        html.Div(id='page-1-content'),
+        html.Br(),
+        dcc.Link('Go to Report', href='/page-2'),
+        html.Br(),
+        dcc.Link('Go back to home', href='/'),
+    ],
+    # I added this style attribute
+    style={'display': 'block', 'line-height': '0', 'height': '0', 'overflow': 'hidden'}
+
+)
+
+page_2_layout = html.Div(
+    # I added this id attribute
+    id='page_2_layout',
+    children=[
+        html.H1('Report Page'),
+        html.Div(id='page-2-content'),
+        html.Br(),
+        dcc.Link('Go to Main Page', href='/page-1'),
+        html.Br(),
+        dcc.Link('Go back to home', href='/'),
+    ],
+    # I added this style attribute
+    style={'display': 'block', 'line-height': '0', 'height': '0', 'overflow': 'hidden'}
+)
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
+    html.Div(id='page-content',
+             # I added this children attribute
+             children=[index_page, page_1_layout, page_2_layout]
+             )
 ])
 
 
-index_page = html.Div([
-    dcc.Link('Main Menu', href='/page-1'),
-    html.Br(),
-    dcc.Link('Results Page', href='/page-2'),
-])
-
-page_1_layout = html.Div([
-    html.H1('Main Menu'),
-    html.H4("Insert a value between 0 - 1000 below"),
-    html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='', type='number', min=0, max=1000, persistence=True, persistence_type='memory')
-    ]),
-    html.Br(),
-    html.Div(id='my-output'),
-    html.Div(id='page-1-content'),
-    html.Br(),
-    dcc.Link('Go to Results Page', href='/page-2'),
-    html.Br(),
-    dcc.Link('Home', href='/'),
-])
-
-
+# Update the index
 @app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
-def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+    [dash.dependencies.Output(page, 'style') for page in ['index_page', 'page_1_layout', 'page_2_layout']],
+    # I turned the output into a list of pages
+    [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    return_value = [{'display': 'block', 'line-height': '0', 'height': '0', 'overflow': 'hidden'} for _ in range(3)]
+
+    if pathname == '/page-1':
+        return_value[1] = {'height': 'auto', 'display': 'inline-block'}
+        return return_value
+    elif pathname == '/page-2':
+        return_value[2] = {'height': 'auto', 'display': 'inline-block'}
+        return return_value
+    else:
+        return_value[0] = {'height': 'auto', 'display': 'inline-block'}
+        return return_value
 
 
 @app.callback(dash.dependencies.Output('page-1-content', 'children'),
-              [dash.dependencies.Input('page-1-dropdown', 'value')])
+              [dash.dependencies.Input('my-input', 'value')])
 def page_1_dropdown(value):
     return 'You have selected "{}"'.format(value)
 
 
-page_2_layout = html.Div([
-    html.H1('Results Page'),
-
-    html.Div(id='page-2-content'),
-    html.Br(),
-    dcc.Link('Main Menu', href='/page-1'),
-    html.Br(),
-    dcc.Link('Home', href='/')
-])
-
-@app.callback(dash.dependencies.Output('page-2-content', 'children'),
-              [dash.dependencies.Input('page-2-radios', 'value')])
-def page_2_radios(value):
-    return 'You have selected {}'.format(value)
-
-
-# Update the index
-@app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page-1':
-        return page_1_layout
-    elif pathname == '/page-2':
-        return page_2_layout
-    else:
-        return index_page
+@app.callback(Output('page-2-content', 'children'),
+              [Input('my-input', 'value')])
+def page_2(value):
+    return 'The all important value driving our business decisions is "{}"'.format(value)
 
 
 if __name__ == '__main__':
